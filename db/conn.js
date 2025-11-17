9cdbd59524e13a7cda4a2bd99718bf87760abd91
EhteshamButt <ehteshambutt58@gmail.com>
2025-11-17 02:49:01 -0800

    as

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     mongoose
-        .connect('mongodb+srv://buttehtesham86:dbottomon123@dbottomon.aetwhlx.mongodb.net/',  {
+        .connect('mongodb+srv://ehteshambutt58:4G9PFxLyIR6PqGLn@cluster0.mw68zmh.mongodb.net/number_discussion?retryWrites=true&w=majority',  {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             
